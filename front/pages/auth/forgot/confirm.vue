9d1b924e3c0d8b3553256f87d0277091b544400a
Aldarund <shaper.int@gmail.com>
2018-09-14 01:06:30 +0300

    forgot password flow

---
+++ front/pages/auth/forgot/confirm.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-form>
+    <v-container>
+      <v-layout row>
+        <v-flex>
+          <p class="dash-trailing">Forgot Password.</p>
+        </v-flex>
+      </v-layout>
+      <v-layout row>
+        <v-flex>
+          <v-text-field v-model="username" label="username" />
+        </v-flex>
+      </v-layout>
+      <v-btn @click="login">Forgot Password</v-btn>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    username: ''
+  }),
+  mounted() {},
+  methods: {
+    async forgotPassword() {
+      await this.$store.dispatch('cognito/forgotPassword', {
+        username: this.username
+      })
+    }
+  }
+}
+</script>
