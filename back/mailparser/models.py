9004c2421102755116d144abc8c71b5a6709542a
Aldarund <shaper.int@gmail.com>
2018-09-17 00:25:51 +0300

    fixes to dynamo data and other serverless fixes

--- back/mailparser/models.py
+++ back/mailparser/models.py
@@ -5,17 +5,19 @@ from pynamodb.attributes import UnicodeAttribute, BooleanAttribute
 class IdeaModel(Model):
     class Meta:
         table_name = "ideas"
+        host = "http://localhost:4570"
     ideaId = UnicodeAttribute(hash_key=True)
-    content = UnicodeAttribute(range_key=True)
+    content = UnicodeAttribute()
     title = UnicodeAttribute()
-    userId = UnicodeAttribute()
+    userId = UnicodeAttribute(range_key=True)
 
 
 class UserModel(Model):
     class Meta:
         table_name = "users"
+        host = "http://localhost:4570"
     userId = UnicodeAttribute(hash_key=True)
     email = UnicodeAttribute()
-    sortKey = UnicodeAttribute(range_key=True)
+    # sortKey = UnicodeAttribute(range_key=True)
     createdDate = UnicodeAttribute()
     firstLogin = BooleanAttribute()
