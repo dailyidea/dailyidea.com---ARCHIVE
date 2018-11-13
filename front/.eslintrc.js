d8f73b9c080d22e0d79ed550ed825710356c40a5
Aldarund <aldarund@gmail.com>
2018-11-13 17:15:15 +0300

    chore deps

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
   rules: {
     'prettier/prettier': 'error',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
 
   },
 
