1f92169f1fe808097b259efac07ad2391f2a6f25
rockstar123 <ravipatel.ce@gmail.com>
2019-07-05 11:55:30 +0530

    Finished home page design.

--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-app>
-    <v-toolbar app flat absolute>
+  <v-app id="app">
+    <v-toolbar class="toolBar" app flat absolute>
       <v-toolbar-title class="blue--text subheading">
         <nuxt-link :to="{ name: 'index' }">
           <strong class="pl-3">Daily Idea</strong>
@@ -8,8 +8,12 @@
       </v-toolbar-title>
       <v-spacer />
       <template v-if="!$store.getters['cognito/isLoggedIn']">
-        <nuxt-link :to="{ name: 'auth-login' }">Login</nuxt-link>
-        <nuxt-link :to="{ name: 'auth-signup' }" class="pl-3">Signup</nuxt-link>
+        <nuxt-link class="helpLink" :to="{ name: 'auth-login' }"
+          >Help</nuxt-link
+        >
+        <nuxt-link class="userLink" :to="{ name: 'auth-signup' }">
+          <v-icon>fa-user</v-icon>
+        </nuxt-link>
       </template>
       <template v-else>
         <a @click="logout">Logout</a>
@@ -23,9 +27,9 @@
       </v-layout>
       <vue-snotify />
     </v-content>
-    <v-footer app>
+    <!-- <v-footer app>
       <span>Dailyidea &copy; 2018</span>
-    </v-footer>
+    </v-footer> -->
   </v-app>
 </template>
 <script>
@@ -37,3 +41,26 @@ export default {
   }
 }
 </script>
+<style lang="scss">
+#app {
+  .toolBar {
+    background: white !important;
+    color: #c0b7c5 !important;
+    font-style: none !important;
+    text-decoration: none !important;
+    .helpLink {
+      color: #c0b7c5 !important;
+      font-style: none !important;
+      text-decoration: none !important;
+    }
+
+    i {
+      color: #c0b7c5 !important;
+      font-style: none !important;
+      text-decoration: none !important;
+      font-size: 15px !important;
+      margin-left: 17px;
+    }
+  }
+}
+</style>
--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="homePage">
-    <div id="firstSection">
+    <!-- First Section -->
+    <div class="firstSection">
       <!-- Desktop - Hero Message & Signup Button -->
-      <v-layout class="desktopHero" hidden-sm-and-down row>
-        <!-- Desktop Hero -->
-        <v-flex hidden-sm-and-down xs12 md6 lg5 offset-md1 offset-lg2>
+
+      <v-layout class="desktopHero" fluid grid-list-md hidden-sm-and-down row>
+        <v-flex hidden-sm-and-down md7 lg6>
           <div class="heroTitle">
             Your Idea
             <br />Could Be Huge!
@@ -53,7 +54,14 @@
       </v-layout>
 
       <!-- Desktop Reviews -->
-      <v-layout hidden-sm-and-down xs12 class="desktopReviews" row>
+      <v-layout
+        hidden-sm-and-down
+        fluid
+        grid-list-md
+        xs12
+        class="desktopReviews"
+        row
+      >
         <v-flex md4 lg4>
           <v-card color="white" class="review">
             Excepteur sint occaecat cuidatat non proident, sunt in culpa qui
@@ -150,6 +158,52 @@
         </v-carousel>
       </v-layout>
     </div>
+
+    <!-- Second Section -->
+    <div class="secondSection">
+      <!-- Desktop View -->
+      <v-container class="desktopView" hidden-sm-and-down>
+        <v-layout row wrap>
+          <!-- Left side image -->
+          <v-flex xs7>
+            &nbsp;
+          </v-flex>
+          <!-- Right side description -->
+          <v-flex xs5 md5>
+            <div class="ttle">How it works</div>
+            <div class="subTitle">Oh, it's easy!</div>
+            <div class="description">
+              We'll send you an email every day with a reminder for you to
+              submit an idea. Just hit reply, and you'll never again.
+            </div>
+            <v-btn class="buttn" outline>More</v-btn>
+          </v-flex>
+        </v-layout>
+      </v-container>
+
+      <!-- Mobile View -->
+      <v-container class="mobileView" grid-list-md hidden-md-and-up>
+        <div class="ttle">How it works</div>
+        <div class="subTitle">Oh, it's easy!</div>
+        <div class="description">
+          We'll send you an email every day with a reminder for you to submit an
+          idea. Just hit reply, and you'll never again.
+        </div>
+        <v-btn class="buttn" outline>More</v-btn>
+      </v-container>
+    </div>
+
+    <!-- Footer -->
+    <div class="homeFooter">
+      <v-layout row wrap>
+        <v-flex md10 lg10 hidden-sm-and-down> &nbsp;</v-flex>
+        <v-flex md2 lg2>
+          <i class="fab fa-instagram"></i>
+          <i class="fab fa-twitter"></i>
+          <i class="fab fa-facebook-f"></i>
+        </v-flex>
+      </v-layout>
+    </div>
   </div>
 </template>
 
@@ -174,44 +228,45 @@ export default {
 
 <style lang="scss">
 #homePage {
-  #firstSection {
+  .firstSection {
     width: 100%;
     min-height: 100vh;
     text-align: left;
     background: #ffc61e;
+    padding-bottom: 8vh;
 
     .desktopHero {
+      padding-left: 11.5%;
       .heroTitle {
-        font-size: 70px;
-        line-height: 85px;
-        letter-spacing: 1px;
+        font-size: 4vw;
+        line-height: 4.5vw;
         padding-top: 7vh;
         font-weight: bold;
       }
 
       .heroSubtitle {
-        font-size: 30px;
+        font-size: 1.6vw;
         color: black;
         font-weight: bold;
-        margin-top: 20px;
+        margin-top: 3vh;
         padding-left: 5px;
       }
 
       .heroDescription {
         padding-left: 5px;
         color: white;
-        font-size: 20px;
-        padding-top: 16px;
-        width: 52%;
+        font-size: 1.2vw;
+        padding-top: 2vh;
+        width: 60%;
         text-align: left;
         letter-spacing: 1px;
-        font-weight: lighter;
+        // font-weight: lighter;
       }
 
       .signupButton {
         width: 220px;
         font-size: 13px;
-        margin-top: 40px;
+        margin-top: 7vh;
       }
     }
 
@@ -220,9 +275,9 @@ export default {
       width: 85%;
       margin: auto;
       .heroTitle {
-        font-size: 50px;
+        font-size: 40px;
         width: 70%;
-        line-height: 55px;
+        line-height: 40px;
         letter-spacing: 1px;
         padding-top: 7vh;
         font-weight: bold;
@@ -240,12 +295,12 @@ export default {
       .heroDescription {
         padding-left: 5px;
         color: white;
-        font-size: 13px;
+        font-size: 14px;
         padding-top: 16px;
         width: 54%;
         text-align: left;
         letter-spacing: 1px;
-        font-weight: lighter;
+        // font-weight: lighter;
       }
 
       .signupButton {
@@ -257,18 +312,19 @@ export default {
 
     .desktopReviews {
       padding-left: 10px;
-      width: 70%;
+      width: 80%;
       margin: auto;
       margin-top: 7vh;
 
       .review {
         max-width: 400px !important;
+        width: 90%;
         padding: 40px 33px;
-        font-size: 20px;
+        font-size: 16px;
         margin: auto;
 
         .reviewerInfo {
-          font-size: 14px;
+          font-size: 13px;
           color: #777;
           line-height: 30px;
           margin-top: 30px;
@@ -290,12 +346,15 @@ export default {
       // border: 1px solid red;
       width: 90%;
       margin: auto;
-      max-height: 27vh;
+      max-height: 33vh;
       background: white;
-      margin-top: 20px;
-      margin-bottom: 30px;
+      margin-top: 25px;
+      padding-bottom: 10px;
+
       .v-carousel__controls {
         background: white;
+        padding-bottom: 5vh !important;
+        padding-top: 4vh !important;
 
         .v-icon {
           color: #777 !important;
@@ -313,11 +372,11 @@ export default {
 
       .review {
         padding: 25px 23px;
-        font-size: 16px;
+        font-size: 18px;
         margin: auto;
 
         .reviewerInfo {
-          font-size: 14px;
+          font-size: px;
           color: #777;
           line-height: 30px;
           margin-top: 20px;
@@ -333,6 +392,98 @@ export default {
           }
         }
       }
+    }
+  }
+
+  .secondSection {
+    text-align: left;
+    padding-top: 5vh;
+
+    .desktopView {
+      .ttle {
+        color: #ffc61e;
+        font-style: italic;
+        font-size: 2vw;
+        line-height: 2.3vw;
+      }
+
+      .subTitle {
+        font-size: 3vw;
+      }
+
+      .description {
+        width: 80%;
+        padding-top: 3vh;
+        font-size: 1.2vw;
+        line-height: 2.2vw;
+        letter-spacing: 1px;
+        padding-left: 5px;
+      }
+    }
+
+    .desktopView {
+      .ttle {
+        color: #ffc61e;
+        font-style: italic;
+        font-size: 2vw;
+        line-height: 2.3vw;
+      }
+
+      .subTitle {
+        font-size: 4vw;
+      }
+      .description {
+        width: 80%;
+        padding-top: 4vh;
+        font-size: 1.3vw;
+        padding-left: 5px;
+        color: #aca7ae;
+      }
+      .buttn {
+        margin-top: 7vh;
+        margin-bottom: 5vh;
+      }
+    }
+
+    .mobileView {
+      padding-left: 8%;
+
+      .ttle {
+        color: #ffc61e;
+        font-style: italic;
+        font-size: 4vw;
+        line-height: 4vw;
+      }
+
+      .subTitle {
+        font-size: 9vw;
+      }
+      .description {
+        width: 70%;
+        padding-top: 3vh;
+        font-size: 4vw;
+        padding-left: 5px;
+        color: #aca7ae;
+      }
+
+      .buttn {
+        margin-top: 30vh;
+      }
+    }
+
+    .buttn {
+      color: #3a1852 !important;
+      border: 2px solid #cbc4cf !important;
+    }
+  }
+
+  .homeFooter {
+    background: #35124e;
+    color: white;
+    padding: 20px;
+    i {
+      margin-left: 13px;
+      margin-right: 13px;
     }
   }
 }
