cd38b4ea4716ae8bc54b2c4c9d4cf403fc9508fb
Ravi <ravi@Ravis-MacBook-Pro.local>
2019-07-16 09:38:11 +0530

    Integrated login page.

--- front/pages/auth/login.vue
+++ front/pages/auth/login.vue
@@ -1,40 +1,81 @@
 <template>
-  <v-form>
-    <v-container>
-      <v-layout row>
-        <v-flex>
-          <p class="dash-trailing">Get Started With A Free Account.</p>
-        </v-flex>
-      </v-layout>
-      <v-layout row>
-        <v-flex>
-          <p>
-            To log in to your email account, enter the email address for your
-            account below.
-          </p>
-          <p>
-            You'll get an email that contains a login link that will
-            automatically log you in to the site.
-          </p>
-        </v-flex>
-      </v-layout>
-      <v-layout row>
-        <v-flex>
+  <div id="loginPage">
+    <v-layout row>
+      <v-flex class="lefgImgContainer" hidden-sm-and-down>
+        <img class="imgPersonWithPhone" src="~/assets/images/person_with_phone.png" />
+      </v-flex>
+
+      <!-- Register Div -->
+      <v-flex class="registerDiv">
+        <img class="logoIcon" src="~/assets/images/signup/dark_gray_lamp.png" />
+        <br />
+        <img class="logoText" src="~/assets/images/logo_text.png" />
+
+        <!-- Login Form -->
+        <v-form>
+          <!-- Email Input Box -->
+          <!-- <validate-text-field
+            v-model="email"
+            class="emailInput"
+            single-line
+            flat
+            label="Enter email"
+            prepend-inner-icon="email"
+          ></validate-text-field>-->
+
           <validate-text-field
             :value.sync="email"
+            single-line
+            flat
+            class="emailInput"
             name="email"
             autocomplete="email"
             type="email"
-            placeholder="null@example.com"
+            prepend-inner-icon="email"
+            placeholder="Enter Email"
             validate="required|email"
           />
-        </v-flex>
-      </v-layout>
-      <v-layout>
-        <v-btn block @click="actionValidate(login)">Login</v-btn>
-      </v-layout>
-    </v-container>
-  </v-form>
+
+          <!-- Continue Button -->
+          <v-btn large class="continueBtn" @click="login">Continue</v-btn>
+        </v-form>
+
+        <!-- Social Login Icons -->
+        <div class="socialIconContainer">
+          <v-btn small outline fab color="primary">
+            <v-icon>fab fa-facebook-f</v-icon>
+          </v-btn>
+          <v-btn small outline fab color="primary">
+            <v-icon>fab fa-twitter</v-icon>
+          </v-btn>
+          <v-btn small outline fab color="primary">
+            <v-icon>fab fa-google-plus-g</v-icon>
+          </v-btn>
+        </div>
+
+        <!-- Login div at bottom -->
+        <div class="loginDiv">
+          <div class="loginTitle">Haven't created account yet?</div>
+          <v-btn large class="loginBtn" color="primary">REgister Now</v-btn>
+        </div>
+      </v-flex>
+
+      <v-flex class="rightImgContainer" hidden-sm-and-down>
+        <img class="imgLightGrayLamp" src="~/assets/images/signup/light_gray_lamp.png" />
+        <img class="imgPersonWithPhone" src="~/assets/images/signup/lady_with_phone.png" />
+      </v-flex>
+    </v-layout>
+
+    <!-- Fixed Footer -->
+    <v-layout
+      hidden-sm-and-down
+      class="fixedFooter"
+      :style="{
+        'background-image':
+          'url(' + require('~/assets/images/signup/footer_background.png') + ')'
+      }"
+    ></v-layout>
+  </div>
 </template>
 
 <script>
@@ -73,3 +114,171 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+@import './assets/style/variables.scss';
+
+#loginPage {
+  // border: 1px solid red;
+  height: 90vh;
+  overflow: hidden;
+  background: white;
+  overflow: hidden;
+  .lefgImgContainer {
+    position: relative;
+    z-index: 10;
+    .imgPersonWithPhone {
+      height: 65vh;
+      position: absolute;
+      right: 0;
+      bottom: 0;
+    }
+  }
+
+  .rightImgContainer {
+    position: relative;
+    z-index: 10;
+
+    .imgLightGrayLamp {
+      height: 50vh;
+      position: absolute;
+      left: 10%;
+      top: 0;
+    }
+
+    .imgPersonWithPhone {
+      height: 65vh;
+      position: absolute;
+      left: 0;
+      bottom: 0;
+    }
+  }
+
+  .registerDiv {
+    // border: 1px solid red;
+    text-align: center;
+    padding-top: 4vh;
+    z-index: 10;
+    height: 85vh;
+    overflow: hidden;
+
+    @media #{$small-screen} {
+      padding-top: 10vh;
+    }
+
+    .logoIcon {
+      width: 70px;
+      @media #{$small-screen} {
+        // padding-top: 30vh;
+        // background: red !important;
+        height: 15vh !important;
+        width: auto !important;
+      }
+    }
+
+    .logoText {
+      width: 200px;
+    }
+
+    .emailInput {
+      margin-top: 7vh !important;
+      margin-bottom: 20px;
+      .v-input__prepend-inner {
+        padding-right: 15px;
+      }
+    }
+
+    .emailInput {
+      width: 70%;
+      margin: auto;
+      max-width: 400px;
+
+      @media #{$medium-screen} {
+        max-width: none;
+        width: 80%;
+      }
+    }
+
+    .continueBtn {
+      border-radius: 4px;
+      background-image: linear-gradient(to left, #ffdf01, #ffb92d);
+      color: white;
+      width: 70%;
+
+      letter-spacing: 1px;
+
+      @media #{$medium-screen} {
+        max-width: none;
+        width: 80%;
+      }
+    }
+
+    .socialIconContainer {
+      margin-top: 6vh;
+      button {
+        border: 1px solid #ebe7ed;
+      }
+    }
+
+    .loginDiv {
+      margin-top: 5vh;
+      .loginTitle {
+        font-size: 14px;
+        font-weight: normal;
+        font-style: normal;
+        font-stretch: normal;
+        line-height: 1.57;
+        letter-spacing: normal;
+        text-align: center;
+        color: #c8c7c7;
+      }
+      .loginBtn {
+        width: 70%;
+        max-width: 400px;
+        border-radius: 4px;
+        letter-spacing: 1px;
+      }
+    }
+
+    @media #{$small-screen} {
+      .loginDiv {
+        position: fixed;
+        bottom: 0px;
+        width: 100%;
+
+        .logoIcon {
+          height: 26vh !important;
+        }
+
+        .logoText {
+          width: 200px;
+        }
+
+        .loginBtn {
+          width: 100%;
+          max-width: none;
+          margin: 0px;
+          border-radius: 0px;
+          margin-top: 10px;
+          height: 64px;
+          letter-spacing: 2px;
+        }
+      }
+    }
+  }
+
+  .fixedFooter {
+    height: 30vh;
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    z-index: 0;
+    // border: 1px solid red;
+
+    background-size: cover;
+    // background-position-y: 30px;
+  }
+}
+</style>
+
