664022d0304858ceef43b7b60c2e1c14be19249e
Aldarund <aldarund@gmail.com>
2019-10-08 02:32:05 +0300

    profile api

---
+++ front/graphql/query/userInfo.js
@@ -0,0 +1,15 @@
+export default `
+query userInfo($userId: String!) {
+  userInfo(userId: $userId) {
+    result {
+      ok
+      error
+    }
+    userInfo {
+      userId
+      followersCount
+      followeesCount
+      ideasCreated
+    }    
+  }
+}`
