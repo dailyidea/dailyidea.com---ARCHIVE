54e14d9965619ab381cce8a91cb7498eb3bda1e3
Ravi Patel <ravipatel.ce@gmail.com>
2019-11-06 16:59:18 +0530

    Lint fix.

--- front/pages/auth/signup/confirmation.vue
+++ front/pages/auth/signup/confirmation.vue
@@ -1,230 +1,246 @@
 <template>
-	<div id="signupConfirmedPage">
-		<!-- Main Grid -->
-		<v-layout row class="gridContainer">
-			<!-- Desktop Only - Left Side Image Container -->
-			<v-flex class="leftSideImageContainer" hidden-sm-and-down>
-				<img class="imgBigTree" src="~/assets/images/bigTree.png" />
-				<img class="imgPersonWithPhone" src="~/assets/images/person_with_phone.png" />
-			</v-flex>
+  <div id="signupConfirmedPage">
+    <!-- Main Grid -->
+    <v-layout row class="gridContainer">
+      <!-- Desktop Only - Left Side Image Container -->
+      <v-flex class="leftSideImageContainer" hidden-sm-and-down>
+        <img class="imgBigTree" src="~/assets/images/bigTree.png" />
+        <img
+          class="imgPersonWithPhone"
+          src="~/assets/images/person_with_phone.png"
+        />
+      </v-flex>
 
-			<!-- Centered  Div -->
-			<v-flex class="successMessage">
-				<!-- Logo Icons -->
-				<div class="logoContainer">
-					<img class="logoIcon" src="~/assets/images/bulb_with_light_holder.png" />
-					<img class="logoText" src="~/assets/images/logo_text.png" />
-				</div>
+      <!-- Centered  Div -->
+      <v-flex class="successMessage">
+        <!-- Logo Icons -->
+        <div class="logoContainer">
+          <img
+            class="logoIcon"
+            src="~/assets/images/bulb_with_light_holder.png"
+          />
+          <img class="logoText" src="~/assets/images/logo_text.png" />
+        </div>
 
-				<!-- Hero Message -->
-				<div class="heroMsg">Welcome!</div>
+        <!-- Hero Message -->
+        <div class="heroMsg">Welcome!</div>
 
-				<!-- Hero Description -->
-				<div class="heroDescription">
-					Daily Idea lets you browse other people's ideas but also save your
-					own. What would you like to do?
-				</div>
+        <!-- Hero Description -->
+        <div class="heroDescription">
+          Daily Idea lets you browse other people's ideas but also save your
+          own. What would you like to do?
+        </div>
 
-				<!-- Action Buttons -->
-				<div class="actionButtonContainer">
-					<v-btn large class="actionBtn" to="/ideas/create">Save a new idea</v-btn>
-					<v-btn large dark class="actionBtn" to="/ideas/all">browse public ideas</v-btn>
-				</div>
+        <!-- Action Buttons -->
+        <div class="actionButtonContainer">
+          <v-btn large class="actionBtn" to="/ideas/create"
+            >Save a new idea</v-btn
+          >
+          <v-btn large dark class="actionBtn" to="/ideas/all"
+            >browse public ideas</v-btn
+          >
+        </div>
+      </v-flex>
 
-			</v-flex>
+      <!-- Right side images -->
+      <v-flex class="rightImgContainer" hidden-sm-and-down>
+        <img class="smallTreeImage" src="~/assets/images/smallTree.png" />
+        <img
+          class="imgPersonWithPhone"
+          src="~/assets/images/signup/lady_with_phone.png"
+        />
+      </v-flex>
+    </v-layout>
 
-			<!-- Right side images -->
-			<v-flex class="rightImgContainer" hidden-sm-and-down>
-				<img class="smallTreeImage" src="~/assets/images/smallTree.png" />
-				<img class="imgPersonWithPhone" src="~/assets/images/signup/lady_with_phone.png" />
-			</v-flex>
-		</v-layout>
-
-		<!-- Fixed Footer -->
-		<v-layout hidden-sm-and-down class="fixedFooter" :style="{
+    <!-- Fixed Footer -->
+    <v-layout
+      hidden-sm-and-down
+      class="fixedFooter"
+      :style="{
         'background-image':
           'url(' + require('~/assets/images/signup/footer_background.png') + ')'
-      }"></v-layout>
-	</div>
+      }"
+    ></v-layout>
+  </div>
 </template>
 
 <script>
 export default {
-	data: () => ({}),
-	mounted() {},
-	methods: {}
+  data: () => ({}),
+  mounted() {},
+  methods: {}
 }
 </script>
 
 <style lang="scss">
 #signupConfirmedPage {
-	// border: 1px solid red;
-	height: 100vh;
-	overflow: hidden;
-	background: white;
-	overflow: hidden;
+  // border: 1px solid red;
+  height: 100vh;
+  overflow: hidden;
+  background: white;
+  overflow: hidden;
 
-	.backBtn {
-		z-index: 100;
-		color: $primary-color;
-		position: absolute;
-		padding: 25px;
+  .backBtn {
+    z-index: 100;
+    color: $primary-color;
+    position: absolute;
+    padding: 25px;
 
-		i {
-			font-size: 16px;
-		}
-	}
+    i {
+      font-size: 16px;
+    }
+  }
 
-	.gridContainer {
-		height: 100vh;
+  .gridContainer {
+    height: 100vh;
 
-		.leftSideImageContainer {
-			position: relative;
-			z-index: 10;
+    .leftSideImageContainer {
+      position: relative;
+      z-index: 10;
 
-			.imgBigTree {
-				height: 75vh;
-				position: absolute;
-				left: -2%;
-				top: 20vh;
-			}
+      .imgBigTree {
+        height: 75vh;
+        position: absolute;
+        left: -2%;
+        top: 20vh;
+      }
 
-			.imgPersonWithPhone {
-				height: 70vh;
-				position: absolute;
-				right: 0;
-				left: 52%;
-				bottom: 5vh;
-			}
-		}
+      .imgPersonWithPhone {
+        height: 70vh;
+        position: absolute;
+        right: 0;
+        left: 52%;
+        bottom: 5vh;
+      }
+    }
 
-		.rightImgContainer {
-			position: relative;
-			z-index: 10;
+    .rightImgContainer {
+      position: relative;
+      z-index: 10;
 
-			.smallTreeImage {
-				height: 85vh;
-				position: absolute;
-				left: 50%;
-				bottom: 10vh;
-			}
+      .smallTreeImage {
+        height: 85vh;
+        position: absolute;
+        left: 50%;
+        bottom: 10vh;
+      }
 
-			.imgPersonWithPhone {
-				height: 70vh;
-				position: absolute;
-				left: 10%;
-				bottom: 5vh;
-			}
-		}
+      .imgPersonWithPhone {
+        height: 70vh;
+        position: absolute;
+        left: 10%;
+        bottom: 5vh;
+      }
+    }
 
-		.successMessage {
-			max-width: 700px;
-			text-align: center;
-			padding-top: 14vh;
-			z-index: 10;
-			padding-left: 10%;
-			padding-right: 10%;
-			// border: 1px solid red;
+    .successMessage {
+      max-width: 700px;
+      text-align: center;
+      padding-top: 14vh;
+      z-index: 10;
+      padding-left: 10%;
+      padding-right: 10%;
+      // border: 1px solid red;
 
-			overflow: hidden;
+      overflow: hidden;
 
-			@media #{$small-screen} {
-				padding-top: 10vh;
-			}
+      @media #{$small-screen} {
+        padding-top: 10vh;
+      }
 
-			.logoContainer {
-				// width: 100%;
-				text-align: center;
-				.logoIcon {
-					height: 17vh;
-					display: block;
-					margin: auto;
+      .logoContainer {
+        // width: 100%;
+        text-align: center;
+        .logoIcon {
+          height: 17vh;
+          display: block;
+          margin: auto;
 
-					@media #{$small-screen} {
-						// padding-top: 30vh;
-						// background: red !important;
-						height: 20vh !important;
-						width: auto !important;
-					}
-				}
+          @media #{$small-screen} {
+            // padding-top: 30vh;
+            // background: red !important;
+            height: 20vh !important;
+            width: auto !important;
+          }
+        }
 
-				.logoText {
-					height: 40px;
-					display: block;
-					margin: auto;
-				}
-			}
+        .logoText {
+          height: 40px;
+          display: block;
+          margin: auto;
+        }
+      }
 
-			.heroMsg {
-				margin-top: 10vh;
-				font-size: 35px;
-				line-height: 1.66;
-				color: #18141c;
+      .heroMsg {
+        margin-top: 10vh;
+        font-size: 35px;
+        line-height: 1.66;
+        color: #18141c;
 
-				@media #{$small-screen} {
-					font-size: 35px;
-				}
-			}
+        @media #{$small-screen} {
+          font-size: 35px;
+        }
+      }
 
-			.heroDescription {
-				margin-top: 2vh;
-				line-height: 1.59;
-				color: #827c85;
+      .heroDescription {
+        margin-top: 2vh;
+        line-height: 1.59;
+        color: #827c85;
 
-				a {
-					font-size: 18px;
-					color: #7777;
-				}
+        a {
+          font-size: 18px;
+          color: #7777;
+        }
 
-				@media #{$small-screen} {
-					margin-top: 30px;
-					font-size: 14px;
-				}
-			}
+        @media #{$small-screen} {
+          margin-top: 30px;
+          font-size: 14px;
+        }
+      }
 
-			.actionButtonContainer {
-				margin-top: 6vh;
+      .actionButtonContainer {
+        margin-top: 6vh;
 
-				.actionBtn {
-					border-radius: 4px;
-					background-image: linear-gradient(to left, #ffdf01, #ffb92d);
-					color: white;
-					width: 246px;
-					margin-bottom: 3vh;
-					letter-spacing: 1px;
+        .actionBtn {
+          border-radius: 4px;
+          background-image: linear-gradient(to left, #ffdf01, #ffb92d);
+          color: white;
+          width: 246px;
+          margin-bottom: 3vh;
+          letter-spacing: 1px;
 
-					@media #{$medium-screen} {
-						width: 130px;
-					}
-				}
-			}
+          @media #{$medium-screen} {
+            width: 130px;
+          }
+        }
+      }
 
-			.browsePublicIdeaBtn {
-				border-radius: 4px;
+      .browsePublicIdeaBtn {
+        border-radius: 4px;
 
-				width: 246px;
-				margin-top: 2vh;
+        width: 246px;
+        margin-top: 2vh;
 
-				letter-spacing: 1px;
+        letter-spacing: 1px;
 
-				@media #{$medium-screen} {
-					width: 130px;
-				}
-			}
-		}
-	}
+        @media #{$medium-screen} {
+          width: 130px;
+        }
+      }
+    }
+  }
 
-	.fixedFooter {
-		height: 30vh;
-		width: 100%;
-		position: fixed;
-		bottom: 0;
-		left: 0;
-		z-index: 0;
-		// border: 1px solid red;
+  .fixedFooter {
+    height: 30vh;
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    z-index: 0;
+    // border: 1px solid red;
 
-		background-size: cover;
-		// background-position-y: 30px;
-	}
+    background-size: cover;
+    // background-position-y: 30px;
+  }
 }
 </style>
--- front/pages/auth/signup/index.vue
+++ front/pages/auth/signup/index.vue
@@ -1,309 +1,341 @@
 <template>
-	<div id="signupPage">
-		<!-- Back button -->
-		<v-btn class="backBtn" text icon color="primary" to="/">
-			<v-icon>fas fa-arrow-left</v-icon>
-		</v-btn>
+  <div id="signupPage">
+    <!-- Back button -->
+    <v-btn class="backBtn" text icon color="primary" to="/">
+      <v-icon>fas fa-arrow-left</v-icon>
+    </v-btn>
 
-		<!-- Grid -->
-		<v-layout class="mainTable" row>
-			<!-- Desktop Only - Left Side Image Container -->
-			<v-flex class="leftSideImageContainer" hidden-sm-and-down>
-				<img class="bigTreeImage" src="~/assets/images/bigTree.png" />
-				<img class="imgPersonWithPhone" src="~/assets/images/person_with_phone.png" />
-			</v-flex>
+    <!-- Grid -->
+    <v-layout class="mainTable" row>
+      <!-- Desktop Only - Left Side Image Container -->
+      <v-flex class="leftSideImageContainer" hidden-sm-and-down>
+        <img class="bigTreeImage" src="~/assets/images/bigTree.png" />
+        <img
+          class="imgPersonWithPhone"
+          src="~/assets/images/person_with_phone.png"
+        />
+      </v-flex>
 
-			<!-- Register Div -->
-			<v-flex class="registerDiv">
-				<img class="logoIcon" src="~/assets/images/signup/dark_gray_lamp.png" />
-				<br />
-				<img class="logoText" src="~/assets/images/logo_text.png" />
+      <!-- Register Div -->
+      <v-flex class="registerDiv">
+        <img class="logoIcon" src="~/assets/images/signup/dark_gray_lamp.png" />
+        <br />
+        <img class="logoText" src="~/assets/images/logo_text.png" />
 
-				<!-- Register Form -->
-				<form>
-					<!-- Email Input Box -->
-					<v-text-field v-model="name" v-validate="'required|max:100'" :error-messages="errors.collect('name')" data-vv-name="name" class="inputBox name" single-line flat label="Enter name" prepend-inner-icon="fas fa-user"></v-text-field>
+        <!-- Register Form -->
+        <form>
+          <!-- Email Input Box -->
+          <v-text-field
+            v-model="name"
+            v-validate="'required|max:100'"
+            :error-messages="errors.collect('name')"
+            data-vv-name="name"
+            class="inputBox name"
+            single-line
+            flat
+            label="Enter name"
+            prepend-inner-icon="fas fa-user"
+          ></v-text-field>
 
-					<!-- Email Input Box -->
-					<v-text-field v-model="email" v-validate="'required|email'" class="inputBox email" :error-messages="errors.collect('email')" data-vv-name="email" single-line flat label="Enter email" prepend-inner-icon="email"></v-text-field>
+          <!-- Email Input Box -->
+          <v-text-field
+            v-model="email"
+            v-validate="'required|email'"
+            class="inputBox email"
+            :error-messages="errors.collect('email')"
+            data-vv-name="email"
+            single-line
+            flat
+            label="Enter email"
+            prepend-inner-icon="email"
+          ></v-text-field>
 
-					<!-- Email Already Exists Message -->
-					<div v-if="emailExistsMsg != ''" class="emailExistsMsg">
-						{{ emailExistsMsg }}
-						<div>
-							<v-btn to="/auth/login" text small color="#827C85">Login instead?</v-btn>
-						</div>
-					</div>
+          <!-- Email Already Exists Message -->
+          <div v-if="emailExistsMsg != ''" class="emailExistsMsg">
+            {{ emailExistsMsg }}
+            <div>
+              <v-btn to="/auth/login" text small color="#827C85"
+                >Login instead?</v-btn
+              >
+            </div>
+          </div>
 
-					<!-- Continue Button -->
-					<v-btn large class="continueBtn" @click="signup">Continue</v-btn>
-				</form>
+          <!-- Continue Button -->
+          <v-btn large class="continueBtn" @click="signup">Continue</v-btn>
+        </form>
 
-				<!-- Login div at bottom -->
-				<div class="accountExists">
-					<div class="existsTitle">
-						Already have an account?
-						<nuxt-link class="loginBtn" text to="/auth/login">Login</nuxt-link>
-					</div>
-				</div>
-			</v-flex>
+        <!-- Login div at bottom -->
+        <div class="accountExists">
+          <div class="existsTitle">
+            Already have an account?
+            <nuxt-link class="loginBtn" text to="/auth/login">Login</nuxt-link>
+          </div>
+        </div>
+      </v-flex>
 
-			<!-- Desktop Only - Right Side Image Container -->
-			<v-flex class="rightSideImageContainer" hidden-sm-and-down>
-				<img class="smallTreeImage" src="~/assets/images/smallTree.png" />
-				<img class="imgPersonWithPhone" src="~/assets/images/signup/lady_with_phone.png" />
-			</v-flex>
-		</v-layout>
+      <!-- Desktop Only - Right Side Image Container -->
+      <v-flex class="rightSideImageContainer" hidden-sm-and-down>
+        <img class="smallTreeImage" src="~/assets/images/smallTree.png" />
+        <img
+          class="imgPersonWithPhone"
+          src="~/assets/images/signup/lady_with_phone.png"
+        />
+      </v-flex>
+    </v-layout>
 
-		<!-- Fixed Footer -->
-		<v-layout hidden-sm-and-down class="fixedFooter" :style="{
+    <!-- Fixed Footer -->
+    <v-layout
+      hidden-sm-and-down
+      class="fixedFooter"
+      :style="{
         'background-image':
           'url(' + require('~/assets/images/signup/footer_background.png') + ')'
-      }"></v-layout>
-	</div>
+      }"
+    ></v-layout>
+  </div>
 </template>
 
 <script>
 import nanoid from 'nanoid'
 import { getErrorMessage } from '~/utils'
 export default {
-	data: () => ({
-		email: '',
-		name: '',
-		emailExistsMsg: ''
-	}),
-	$_veeValidate: {
-		validator: 'new'
-	},
-	mounted() {
-		this.$validator.localize('en', this.dictionary)
-	},
-	methods: {
-		async signup() {
-			try {
-				this.emailExistsMsg = ''
+  data: () => ({
+    email: '',
+    name: '',
+    emailExistsMsg: ''
+  }),
+  $_veeValidate: {
+    validator: 'new'
+  },
+  mounted() {
+    this.$validator.localize('en', this.dictionary)
+  },
+  methods: {
+    async signup() {
+      try {
+        this.emailExistsMsg = ''
 
-				//Validate input fields
-				let result = await this.$validator.validateAll()
-				if (!result) {
-					return
-				}
+        //Validate input fields
+        let result = await this.$validator.validateAll()
+        if (!result) {
+          return
+        }
 
-				await this.$store.dispatch('cognito/registerUser', {
-					username: this.email,
-					password: nanoid(),
-					attributes: {
-						name: this.name
-					}
-				})
+        await this.$store.dispatch('cognito/registerUser', {
+          username: this.email,
+          password: nanoid(),
+          attributes: {
+            name: this.name
+          }
+        })
 
-				// Redirect to registeration success page
-				this.$router.push({
-					name: 'auth-signup-success',
-					params: { email: this.email }
-				})
-			} catch (e) {
-				// Handle email already registered
-				if (e.code && e.code == 'UsernameExistsException') {
-					this.emailExistsMsg = 'Sorry, email mentioned already exist.'
-					return
-				}
+        // Redirect to registeration success page
+        this.$router.push({
+          name: 'auth-signup-success',
+          params: { email: this.email }
+        })
+      } catch (e) {
+        // Handle email already registered
+        if (e.code && e.code == 'UsernameExistsException') {
+          this.emailExistsMsg = 'Sorry, email mentioned already exist.'
+          return
+        }
 
-				console.log('error', e)
-				this.$snotify.error(getErrorMessage(e), 'Error', {
-					timeout: 2000,
-					showProgressBar: false,
-					closeOnClick: true,
-					pauseOnHover: true
-				})
-			}
-		}
-	}
+        console.log('error', e)
+        this.$snotify.error(getErrorMessage(e), 'Error', {
+          timeout: 2000,
+          showProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true
+        })
+      }
+    }
+  }
 }
 </script>
 
 <style lang="scss">
 #signupPage {
-	// border: 1px solid red;
-	height: 100vh;
-	overflow: hidden;
-	background: white;
-	overflow: hidden;
+  // border: 1px solid red;
+  height: 100vh;
+  overflow: hidden;
+  background: white;
+  overflow: hidden;
 
-	.backBtn {
-		color: $primary-color;
-		position: fixed;
-		top: 0;
-		left: 0;
-		margin: 5px 3px;
-		z-index: 5000;
+  .backBtn {
+    color: $primary-color;
+    position: fixed;
+    top: 0;
+    left: 0;
+    margin: 5px 3px;
+    z-index: 5000;
 
-		i {
-			font-size: 16px;
-		}
-	}
+    i {
+      font-size: 16px;
+    }
+  }
 
-	.mainTable {
-		margin: 0px;
-		height: 100vh;
-		z-index: 100;
+  .mainTable {
+    margin: 0px;
+    height: 100vh;
+    z-index: 100;
 
-		.leftSideImageContainer {
-			position: relative;
-			z-index: 10;
+    .leftSideImageContainer {
+      position: relative;
+      z-index: 10;
 
-			.bigTreeImage {
-				height: 75vh;
-				position: absolute;
-				left: -3%;
-				top: 20vh;
-			}
+      .bigTreeImage {
+        height: 75vh;
+        position: absolute;
+        left: -3%;
+        top: 20vh;
+      }
 
-			.imgPersonWithPhone {
-				height: 75vh;
-				position: absolute;
-				right: 10%;
-				bottom: 3vh;
-			}
-		}
+      .imgPersonWithPhone {
+        height: 75vh;
+        position: absolute;
+        right: 10%;
+        bottom: 3vh;
+      }
+    }
 
-		.rightSideImageContainer {
-			position: relative;
-			z-index: 100;
+    .rightSideImageContainer {
+      position: relative;
+      z-index: 100;
 
-			.smallTreeImage {
-				height: 90vh;
-				position: absolute;
-				left: 58%;
-				bottom: 6vh;
-			}
+      .smallTreeImage {
+        height: 90vh;
+        position: absolute;
+        left: 58%;
+        bottom: 6vh;
+      }
 
-			.imgPersonWithPhone {
-				height: 72vh;
-				position: absolute;
-				left: 20%;
-				bottom: 3vh;
-			}
-		}
+      .imgPersonWithPhone {
+        height: 72vh;
+        position: absolute;
+        left: 20%;
+        bottom: 3vh;
+      }
+    }
 
-		.registerDiv {
-			// border: 1px solid red;
-			text-align: center;
-			padding-top: 15vh;
-			z-index: 10;
-			height: 100vh;
-			overflow-x: auto;
+    .registerDiv {
+      // border: 1px solid red;
+      text-align: center;
+      padding-top: 15vh;
+      z-index: 10;
+      height: 100vh;
+      overflow-x: auto;
 
-			@media #{$small-screen} {
-				padding-top: 10vh;
-			}
+      @media #{$small-screen} {
+        padding-top: 10vh;
+      }
 
-			.logoIcon {
-				width: 70px;
-			}
+      .logoIcon {
+        width: 70px;
+      }
 
-			.logoText {
-				width: 200px;
-				margin-bottom: 7vh !important;
-			}
+      .logoText {
+        width: 200px;
+        margin-bottom: 7vh !important;
+      }
 
-			.inputBox {
-				width: 70%;
-				margin: auto;
-				max-width: 420px;
-				margin-top: 0.5vh !important;
+      .inputBox {
+        width: 70%;
+        margin: auto;
+        max-width: 420px;
+        margin-top: 0.5vh !important;
 
-				.v-input__prepend-inner {
-					padding-right: 15px;
-					font-size: 12px;
+        .v-input__prepend-inner {
+          padding-right: 15px;
+          font-size: 12px;
 
-					i {
-						font-size: 18px;
-					}
-				}
-			}
+          i {
+            font-size: 18px;
+          }
+        }
+      }
 
-			.emailExistsMsg {
-				width: 70%;
-				margin: auto;
-				max-width: 420px;
-				color: #c8c7c7;
-			}
+      .emailExistsMsg {
+        width: 70%;
+        margin: auto;
+        max-width: 420px;
+        color: #c8c7c7;
+      }
 
-			.continueBtn {
-				border-radius: 4px;
-				margin-top: 5vh;
-				width: 70%;
-				max-width: 420px;
+      .continueBtn {
+        border-radius: 4px;
+        margin-top: 5vh;
+        width: 70%;
+        max-width: 420px;
 
-				letter-spacing: 1px;
-			}
+        letter-spacing: 1px;
+      }
 
-			.accountExists {
-				margin-top: 6vh;
+      .accountExists {
+        margin-top: 6vh;
 
-				.existsTitle {
-					font-size: 14px;
-					line-height: 1.57;
-					color: #c8c7c7;
+        .existsTitle {
+          font-size: 14px;
+          line-height: 1.57;
+          color: #c8c7c7;
 
-					.loginBtn {
-						text-decoration: none;
-					}
-				}
-			}
+          .loginBtn {
+            text-decoration: none;
+          }
+        }
+      }
 
-			@media #{$small-screen} {
-				position: fixed;
-				bottom: 0px;
-				width: 100%;
+      @media #{$small-screen} {
+        position: fixed;
+        bottom: 0px;
+        width: 100%;
 
-				.logoIcon {
-					height: 20vh !important;
-					width: auto;
-				}
+        .logoIcon {
+          height: 20vh !important;
+          width: auto;
+        }
 
-				.logoText {
-					width: 200px;
-				}
+        .logoText {
+          width: 200px;
+        }
 
-				.inputBox,
-				.continueBtn {
-					width: 80%;
-					max-width: none;
-				}
+        .inputBox,
+        .continueBtn {
+          width: 80%;
+          max-width: none;
+        }
 
-				.accountExists {
-					position: fixed;
-					bottom: 0;
-					width: 100%;
+        .accountExists {
+          position: fixed;
+          bottom: 0;
+          width: 100%;
 
-					.loginBtn {
-						display: block;
-						width: 100%;
-						border-radius: 0px;
-						padding: 20px !important;
-						background-color: $primary-color !important;
-						color: white !important;
-						margin-top: 3px;
-						text-transform: uppercase !important;
-					}
-				}
-			}
-		}
-	}
+          .loginBtn {
+            display: block;
+            width: 100%;
+            border-radius: 0px;
+            padding: 20px !important;
+            background-color: $primary-color !important;
+            color: white !important;
+            margin-top: 3px;
+            text-transform: uppercase !important;
+          }
+        }
+      }
+    }
+  }
 
-	.fixedFooter {
-		height: 30vh;
-		width: 100%;
-		position: fixed;
-		bottom: 0;
-		left: 0;
-		z-index: 0;
-		// border: 1px solid red;
+  .fixedFooter {
+    height: 30vh;
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    z-index: 0;
+    // border: 1px solid red;
 
-		background-size: cover;
-		// background-position-y: 30px;
-	}
+    background-size: cover;
+    // background-position-y: 30px;
+  }
 }
 </style>
