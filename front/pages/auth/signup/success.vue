7a611b6c232537ae2c76779c079dc1eac926a232
Ravi Patel <ravipatel.ce@gmail.com>
2019-11-06 17:07:40 +0530

    Modified signnup success page conntents.

--- front/pages/auth/signup/success.vue
+++ front/pages/auth/signup/success.vue
@@ -1,245 +1,230 @@
 <template>
-  <div id="registerSuccessPage">
-    <!-- Back button -->
-    <v-btn class="backBtn" text icon color="primary" to="/">
-      <v-icon>fas fa-arrow-left</v-icon>
-    </v-btn>
+	<div id="registerSuccessPage">
+		<!-- Back button -->
+		<v-btn class="backBtn" text icon color="primary" to="/">
+			<v-icon>fas fa-arrow-left</v-icon>
+		</v-btn>
 
-    <!-- Main Grid -->
-    <v-layout row class="gridContainer">
-      <!-- Desktop Only - Left Side Image Container -->
-      <v-flex class="leftSideImageContainer" hidden-sm-and-down>
-        <img class="imgBigTree" src="~/assets/images/bigTree.png" />
-        <img
-          class="imgPersonWithPhone"
-          src="~/assets/images/person_with_phone.png"
-        />
-      </v-flex>
+		<!-- Main Grid -->
+		<v-layout row class="gridContainer">
+			<!-- Desktop Only - Left Side Image Container -->
+			<v-flex class="leftSideImageContainer" hidden-sm-and-down>
+				<img class="imgBigTree" src="~/assets/images/bigTree.png" />
+				<img class="imgPersonWithPhone" src="~/assets/images/person_with_phone.png" />
+			</v-flex>
 
-      <!-- Centered  Div -->
-      <v-flex class="successMessage">
-        <!-- Logo Icons -->
-        <div class="logoContainer">
-          <img
-            class="logoIcon"
-            src="~/assets/images/bulb_with_light_holder.png"
-          />
-          <img class="logoText" src="~/assets/images/logo_text.png" />
-        </div>
+			<!-- Centered  Div -->
+			<v-flex class="successMessage">
+				<!-- Logo Icons -->
+				<div class="logoContainer">
+					<img class="logoIcon" src="~/assets/images/bulb_with_light_holder.png" />
+					<img class="logoText" src="~/assets/images/logo_text.png" />
+				</div>
 
-        <!-- Hero Message -->
-        <div class="heroMsg">Hooray!</div>
+				<!-- Hero Message -->
+				<div class="heroMsg">Hooray!</div>
 
-        <!-- Hero Description -->
-        <div class="heroDescription">
-          We've sent you a register link. Click the link to confirm your email
-          and get started!
-        </div>
+				<!-- Hero Description -->
+				<div class="heroDescription">
+					We've sent a magic link to <strong>{{email}}</strong>. Please check your your inbox and click on the email confirmation link in the email to get started!
+				</div>
 
-        <v-btn large class="okBtn" to="/">Okay</v-btn>
-      </v-flex>
+			</v-flex>
 
-      <v-flex class="rightImgContainer" hidden-sm-and-down>
-        <img class="smallTreeImage" src="~/assets/images/smallTree.png" />
+			<v-flex class="rightImgContainer" hidden-sm-and-down>
+				<img class="smallTreeImage" src="~/assets/images/smallTree.png" />
 
-        <img
-          class="imgPersonWithPhone"
-          src="~/assets/images/signup/lady_with_phone.png"
-        />
-      </v-flex>
-    </v-layout>
+				<img class="imgPersonWithPhone" src="~/assets/images/signup/lady_with_phone.png" />
+			</v-flex>
+		</v-layout>
 
-    <!-- Fixed Footer -->
-    <v-layout
-      hidden-sm-and-down
-      class="fixedFooter"
-      :style="{
+		<!-- Fixed Footer -->
+		<v-layout hidden-sm-and-down class="fixedFooter" :style="{
         'background-image':
           'url(' + require('~/assets/images/signup/footer_background.png') + ')'
-      }"
-    ></v-layout>
-  </div>
+      }"></v-layout>
+	</div>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    email: null,
-    success: false
-  }),
-  mounted() {
-    if (this.$route.params.email) {
-      this.email = this.$route.params.email
-    }
-  },
-  methods: {}
+	data: () => ({
+		email: null,
+		success: false
+	}),
+	mounted() {
+		if (this.$route.params.email) {
+			this.email = this.$route.params.email
+		}
+	},
+	methods: {}
 }
 </script>
 
 <style lang="scss">
 #registerSuccessPage {
-  // border: 1px solid red;
-  height: 100vh;
-  overflow: hidden;
-  background: white;
-  overflow: hidden;
+	// border: 1px solid red;
+	height: 100vh;
+	overflow: hidden;
+	background: white;
+	overflow: hidden;
 
-  .backBtn {
-    z-index: 100;
-    color: $primary-color;
-    position: absolute;
-    padding: 25px;
+	.backBtn {
+		z-index: 100;
+		color: $primary-color;
+		position: absolute;
+		padding: 25px;
 
-    i {
-      font-size: 16px;
-    }
-  }
+		i {
+			font-size: 16px;
+		}
+	}
 
-  .gridContainer {
-    height: 100vh;
+	.gridContainer {
+		height: 100vh;
 
-    .leftSideImageContainer {
-      position: relative;
-      z-index: 10;
+		.leftSideImageContainer {
+			position: relative;
+			z-index: 10;
 
-      .imgBigTree {
-        height: 75vh;
-        position: absolute;
-        left: -2%;
-        top: 20vh;
-      }
+			.imgBigTree {
+				height: 75vh;
+				position: absolute;
+				left: -2%;
+				top: 20vh;
+			}
 
-      .imgPersonWithPhone {
-        height: 70vh;
-        position: absolute;
-        right: 0;
-        left: 52%;
-        bottom: 5vh;
-      }
-    }
+			.imgPersonWithPhone {
+				height: 70vh;
+				position: absolute;
+				right: 0;
+				left: 52%;
+				bottom: 5vh;
+			}
+		}
 
-    .rightImgContainer {
-      position: relative;
-      z-index: 10;
+		.rightImgContainer {
+			position: relative;
+			z-index: 10;
 
-      .smallTreeImage {
-        height: 85vh;
-        position: absolute;
-        left: 50%;
-        bottom: 10vh;
-      }
+			.smallTreeImage {
+				height: 85vh;
+				position: absolute;
+				left: 50%;
+				bottom: 10vh;
+			}
 
-      .imgPersonWithPhone {
-        height: 70vh;
-        position: absolute;
-        left: 10%;
-        bottom: 5vh;
-      }
-    }
+			.imgPersonWithPhone {
+				height: 70vh;
+				position: absolute;
+				left: 10%;
+				bottom: 5vh;
+			}
+		}
 
-    .successMessage {
-      max-width: 600px;
-      text-align: center;
-      padding-top: 8vh;
-      z-index: 10;
-      padding-left: 100px;
-      padding-right: 100px;
-      // border: 1px solid red;
+		.successMessage {
+			max-width: 600px;
+			text-align: center;
+			padding-top: 16vh;
+			z-index: 10;
+			padding-left: 100px;
+			padding-right: 100px;
+			// border: 1px solid red;
 
-      overflow: hidden;
+			overflow: hidden;
 
-      @media #{$small-screen} {
-        padding-top: 10vh;
-        padding-left: 10%;
-        padding-right: 10%;
-      }
+			@media #{$small-screen} {
+				padding-top: 15vh;
+				padding-left: 10%;
+				padding-right: 10%;
+			}
 
-      .logoContainer {
-        // width: 100%;
-        text-align: center;
-        .logoIcon {
-          height: 17vh;
-          display: block;
-          margin: auto;
+			.logoContainer {
+				// width: 100%;
+				text-align: center;
+				.logoIcon {
+					height: 17vh;
+					display: block;
+					margin: auto;
 
-          @media #{$small-screen} {
-            // padding-top: 30vh;
-            // background: red !important;
-            height: 20vh !important;
-            width: auto !important;
-          }
-        }
+					@media #{$small-screen} {
+						// padding-top: 30vh;
+						// background: red !important;
+						height: 20vh !important;
+						width: auto !important;
+					}
+				}
 
-        .logoText {
-          height: 40px;
-          display: block;
-          margin: auto;
-        }
-      }
+				.logoText {
+					height: 40px;
+					display: block;
+					margin: auto;
+				}
+			}
 
-      .heroMsg {
-        margin-top: 10vh;
-        font-size: 35px;
-        line-height: 1.66;
-        color: #18141c;
+			.heroMsg {
+				margin-top: 10vh;
+				font-size: 35px;
+				line-height: 1.66;
+				color: #18141c;
 
-        @media #{$small-screen} {
-          font-size: 35px;
-        }
-      }
+				@media #{$small-screen} {
+					font-size: 35px;
+				}
+			}
 
-      .heroDescription {
-        margin-top: 2vh;
-        line-height: 1.59;
-        color: #827c85;
+			.heroDescription {
+				margin-top: 2vh;
+				line-height: 1.59;
+				color: #827c85;
 
-        a {
-          font-size: 18px;
-          color: #7777;
-        }
+				a {
+					font-size: 18px;
+					color: #7777;
+				}
 
-        @media #{$small-screen} {
-          margin-top: 30px;
-          font-size: 14px;
-        }
-      }
+				@media #{$small-screen} {
+					margin-top: 30px;
+					font-size: 14px;
+				}
+			}
 
-      .okBtn {
-        border-radius: 4px;
-        background-image: linear-gradient(to left, #ffdf01, #ffb92d);
-        color: white;
-        width: 246px;
-        margin-top: 4vh;
+			.okBtn {
+				border-radius: 4px;
+				background-image: linear-gradient(to left, #ffdf01, #ffb92d);
+				color: white;
+				width: 246px;
+				margin-top: 4vh;
 
-        letter-spacing: 1px;
+				letter-spacing: 1px;
 
-        @media #{$medium-screen} {
-          width: 130px;
-        }
-      }
+				@media #{$medium-screen} {
+					width: 130px;
+				}
+			}
 
-      .requestLink {
-        margin-top: 30px;
-        font-size: 15px;
-        a {
-          color: #777;
-          text-decoration: underline;
-        }
-      }
-    }
-  }
+			.requestLink {
+				margin-top: 30px;
+				font-size: 15px;
+				a {
+					color: #777;
+					text-decoration: underline;
+				}
+			}
+		}
+	}
 
-  .fixedFooter {
-    height: 30vh;
-    width: 100%;
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    z-index: 0;
-    // border: 1px solid red;
+	.fixedFooter {
+		height: 30vh;
+		width: 100%;
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		z-index: 0;
+		// border: 1px solid red;
 
-    background-size: cover;
-    // background-position-y: 30px;
-  }
+		background-size: cover;
+		// background-position-y: 30px;
+	}
 }
 </style>
