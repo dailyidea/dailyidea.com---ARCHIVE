79981998db3bf3096d26b83b03f12c79c8219caa
Aldarund <aldarund@gmail.com>
2019-11-22 23:52:06 +0300

    add notifier and bookmark components

---
+++ front/components/VisualNotifier.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-snackbar
+    v-model="snackbarVisible"
+    :absolute="true"
+    :timeout="2000"
+    :color="color"
+  >
+    {{ message }}
+    <v-btn color="white" text @click="close">
+      Close
+    </v-btn>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  name: 'SnackBarNotifier',
+  data() {
+    return {
+      message: '',
+      color: '',
+      snackbarVisible: false
+    }
+  },
+  methods: {
+    success(message) {
+      this.message = message
+      this.color = 'success'
+      this.snackbarVisible = true
+    },
+    error(message) {
+      this.message = message
+      this.color = 'error'
+      this.snackbarVisible = true
+    },
+    close() {
+      this.snackbarVisible = false
+    }
+  }
+}
+</script>
