af71ee204bca95ba3dc0c6f2bd5e26c66fdd5ac1
Aldarund <aldarund@gmail.com>
2019-12-05 15:59:08 +0300

    fix send daily idea

--- back/mail-processing/request_daily_idea_user.py
+++ back/mail-processing/request_daily_idea_user.py
@@ -6,8 +6,8 @@ from mail_templates.request_daily_idea.send_request_daily import send_daily
 
 
 def endpoint(event, context):
-    user = UserModel.emailIndex.query(event.email)
-    send_daily(user)
+    user = list(UserModel.emailIndex.query(event.get('email')))
+    send_daily(user[0])
 #
 # if __name__ == '__main__':
 #     endpoint('1','2')
