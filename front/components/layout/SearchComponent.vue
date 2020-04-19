99ed3f1a5b79fc15d33802b912070d8c1e76e6f6
ezl <ericzliu@gmail.com>
2020-04-19 17:58:27 -0500

    keep icon right

--- front/components/layout/SearchComponent.vue
+++ front/components/layout/SearchComponent.vue
@@ -1,36 +1,36 @@
 <template>
-  <div>
-          <!-- when search mode is on, show text input field -->
-          <v-text-field
-            v-if="searchIdeaMode"
-            v-model="label"
-            label="What are you looking for?"
-            solo
-            dense
-            autofocus
-            hide-details
-          >
-            <template v-slot:append>
-              <v-icon @click="toggleSearchIdeaMode">fa-times</v-icon>
-            </template>
-            <template v-slot:prepend-inner>
-              <v-icon size="22" @click="toggleSearchIdeaMode"
-                >fa-search</v-icon
-              >
-            </template>
-          </v-text-field>
+  <div class="text-right">
+    <!-- when search mode is on, show text input field -->
+    <v-text-field
+      v-if="searchIdeaMode"
+      v-model="label"
+      label="What are you looking for?"
+      solo
+      dense
+      autofocus
+      hide-details
+    >
+      <template v-slot:append>
+        <v-icon @click="toggleSearchIdeaMode">fa-times</v-icon>
+      </template>
+      <template v-slot:prepend-inner>
+        <v-icon size="22" @click="toggleSearchIdeaMode"
+          >fa-search</v-icon
+        >
+      </template>
+    </v-text-field>
 
-          <!-- always visible search icon that toggles mode -->
-          <v-btn
-            v-else
-            icon
-            height="36"
-            width="36"
-            :input-value="searchIdeaMode"
-            @click="toggleSearchIdeaMode"
-          >
-            <v-icon>fas fa-search</v-icon>
-          </v-btn>
+    <!-- otherwise the search icon that toggles mode -->
+    <v-btn
+      v-else
+      icon
+      height="36"
+      width="36"
+      :input-value="searchIdeaMode"
+      @click="toggleSearchIdeaMode"
+    >
+      <v-icon>fas fa-search</v-icon>
+    </v-btn>
   </div>
 </template>
 
