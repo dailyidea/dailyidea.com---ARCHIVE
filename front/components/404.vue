e6f84f1fbae686068eeecafdd7c3644968c78a4c
ezl <ericzliu@gmail.com>
2020-04-26 20:22:56 -0500

    404 page

--- front/components/404.vue
+++ front/components/404.vue
@@ -1,37 +1,31 @@
 <template>
-  <v-layout id="pageNotFound">
-    <!--mobile view-->
-    <v-layout inline-block class="mobile" hidden-md-and-up>
-      <div>
-        <img class="imageOfBoy" src="~/assets/images/404.png" />
-        <div class="titleText">
-          404
-        </div>
-        <div class="note2">
-          Grrreat....<br />
-          Now you're stuck.<br />
-          Go back, maybe?
-        </div>
-      </div>
-    </v-layout>
+  <v-layout>
+    <v-container>
+      <v-row>
+        <v-col cols="10" offset="1" md="8" offset-md="2">
+          <v-img
+            src="~/assets/images/undraw_page_not_found_su7k.svg"
+            contain
+            max-width="480"
+            class="mx-auto"
+          ></v-img>
+        </v-col>
+      </v-row>
 
-    <!--desktop view-->
-    <v-layout class="desktop" hidden-sm-and-down>
-      <div class="leftSide">
-        <img class="backgroundLamp" src="~/assets/images/light_gray_lamp.png" />
-        <div class="titleText">
-          404
-        </div>
-        <div class="note2">
-          Grrreat....<br />
-          Now you're stuck.<br />
-          Go back, maybe?
-        </div>
-      </div>
-      <div class="rightSide" style="width:100%;">
-        <img class="imageOfBoy" src="~/assets/images/404.png" />
-      </div>
-    </v-layout>
+      <v-row>
+        <v-col cols="10" offset="1" md="8" offset-md="2" class="justify-center my-4">
+          <h1 class="page-title">Page Not Found</h1>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col cols="10" offset="1" md="8" offset-md="2" class="justify-center my-4 text-center">
+          <p>Oops, sorry. We weren't able to find the page you are looking for.</p>
+          <p>If you typed it in manually, please check the link and try again.</p>
+          <p>Feel free to <a href="/ideas/all/">browse ideas on the site</a> or <a href="/">go to our home page</a>.</p>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-layout>
 </template>
 <script>
@@ -40,86 +34,5 @@ export default {
 }
 </script>
 <style lang="scss">
-#pageNotFound {
-  background: white;
-
-  .desktop {
-    display: flex;
-    min-height: 94vh;
-    width: 100%;
-    overflow: hidden;
-
-    .leftSide {
-      flex: 1;
-
-      .backgroundLamp {
-        padding-top: 10px;
-        position: absolute;
-        width: 300px;
-      }
-
-      .titleText {
-        padding-top: 25vh;
-        padding-left: 10%;
-        text-align: center;
-        font-size: 100px;
-        z-index: 1;
-      }
-
-      .note2 {
-        text-align: center;
-        font-size: 20px;
-        color: #d4cdd7;
-        padding-top: 2vh;
-        padding-left: 10%;
-      }
-    }
-
-    .rightSide {
-      height: 91vh;
-      flex: 0.8;
-
-      .imageOfBoy {
-        width: 100%;
-        max-width: 550px;
-        position: absolute;
-        bottom: 0px;
-        background-color: white;
-
-        @media #{$small-screen} {
-          height: 52vh;
-          position: absolute;
-        }
-      }
-    }
-  }
-
-  .mobile {
-    background: white;
-    min-height: 80vh;
-
-    .imageOfBoy {
-      padding-top: 10px;
-      position: fixed;
-      max-height: 50%;
-      bottom: 0px;
-      background: white;
-    }
-
-    .titleText {
-      padding-top: 3vh;
-      padding-left: 15vh;
-      text-align: center;
-      font-size: 80px;
-    }
-
-    .note2 {
-      text-align: center;
-      padding-left: 15vh;
-      font-size: 20px;
-      color: #d4cdd7;
-      padding-top: 2vh;
-    }
-  }
-}
+@import "~assets/style/common";
 </style>
