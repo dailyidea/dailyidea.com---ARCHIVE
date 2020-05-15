41a4c91694c13f9aa1c945cfec65b3b0484e880c
Frankie Primerano <frankieprimerano@gmail.com>
2020-05-15 16:26:08 -0400

    Issue 315 [WIP] - Replaced hard coded quote in daily_idea.html with a random quote from the google sheet. Has not yet been tested.

--- back/mail-processing/mail_templates/request_daily_idea/send_request_daily.py
+++ back/mail-processing/mail_templates/request_daily_idea/send_request_daily.py
@@ -1,39 +1,54 @@
 import os
+import random
 from datetime import date
 import json
 import boto3
+
+from quotes_list import quotes_list
 
 AWS_REGION = os.environ['SES_AWS_REGION']
 MAILBOX_ADDR = os.environ['MAILBOX_ADDR']
 
 SENDER = f"Daily Idea <{MAILBOX_ADDR}>"
 
+def get_random_quote():
+    return random.choice(quotes_list)
 
 def send_daily_bulk(users_list):
     REQUEST_DAILY_EMAIL_TEMPLATE_NAME = os.environ.get('REQUEST_DAILY_EMAIL_TEMPLATE_NAME')
     BUCKET_URL_PREFIX = os.environ.get('BUCKET_URL_PREFIX')
     DOMAIN_NAME = os.environ.get('DOMAIN_NAME')
     TODAY = date.today().strftime('%a %b %d %Y')
+    
+    destinations = []
+
+    for user in users_list:
+        quote = get_random_quote()
+
+        destinations.append({
+            'Destination': {
+                'ToAddresses': [
+                    user.email,
+                ],
+            },
+            'ReplacementTemplateData': json.dumps(
+                {
+                    "USERNAME": user.name,
+                    "USER_ID": user.userId,
+                    "SNOOZE_TOKEN": user.emailToken,
+                    "QUOTE":quote['quote'],
+                    "QUOTE_BY":quote['by']
+                }
+            )
+        })
+
+    
+    
     client = boto3.client('ses', region_name=AWS_REGION)
     client.send_bulk_templated_email(
             Source=SENDER,
             Template=REQUEST_DAILY_EMAIL_TEMPLATE_NAME,
             DefaultTemplateData=json.dumps(
                 {"BUCKET_URL_PREFIX": BUCKET_URL_PREFIX, "DOMAIN_NAME": DOMAIN_NAME, "TODAY": TODAY, }),
-            Destinations=[
-                {
-                    'Destination': {
-                        'ToAddresses': [
-                            user.email,
-                        ],
-                    },
-                    'ReplacementTemplateData': json.dumps(
-                        {
-                            "USERNAME": user.name,
-                            "USER_ID": user.userId,
-                            "SNOOZE_TOKEN": user.emailToken,
-                        }
-                    )
-                } for user in users_list
-            ]
-        )
+            Destinations=destinations 
+    )
