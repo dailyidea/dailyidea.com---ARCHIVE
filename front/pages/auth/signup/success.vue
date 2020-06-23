73709477d9ed31a1ae1f49d2455b65e1992f871a
Dolidodzik <teethtard321@gmail.com>
2020-06-23 16:32:02 +0000

    #213 - Changed confirmation page text

--- front/pages/auth/signup/success.vue
+++ front/pages/auth/signup/success.vue
@@ -4,9 +4,8 @@
       <div class="heroMsg">Hooray!</div>
       <!-- Hero Description -->
       <div class="heroDescription">
-        We've sent a magic link to <strong>{{ email }}</strong
-        >. Please check your inbox and click on the email confirmation link in
-        the email to get started!
+        We've sent an email to <strong>{{ email }}</strong
+        >, containing instructions to create the account and start sharing ideas.
       </div>
     </div>
   </auth-page>
