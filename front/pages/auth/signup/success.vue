6621bc8ba486b1cab7f124906a3c5732162ab11c
Dolidodzik <teethtard321@gmail.com>
2020-06-26 18:06:52 +0000

    Revert "#213 - Changed confirmation page text"
    This reverts commit 73709477d9ed31a1ae1f49d2455b65e1992f871a.

--- front/pages/auth/signup/success.vue
+++ front/pages/auth/signup/success.vue
@@ -4,8 +4,9 @@
       <div class="heroMsg">Hooray!</div>
       <!-- Hero Description -->
       <div class="heroDescription">
-        We've sent an email to <strong>{{ email }}</strong
-        >, containing instructions to create the account and start sharing ideas.
+        We've sent a magic link to <strong>{{ email }}</strong
+        >. Please check your inbox and click on the email confirmation link in
+        the email to get started!
       </div>
     </div>
   </auth-page>
