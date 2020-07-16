f19d24dad1680ea594fd1b3818efe7d2cd74f6e1
Frankie Primerano <frankieprimerano@gmail.com>
2020-07-15 21:02:08 -0400

    Issue 480 - isMobile mixin

--- front/plugins/mixins.js
+++ front/plugins/mixins.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 
 Vue.mixin({
+  computed: {
+    isMobile() {
+      return !!navigator.share
+    }
+  },
   methods: {
     $scrollToFirstError() {
       const field = this.$validator.errors.items[0].field
