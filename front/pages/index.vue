8ebf3a72422e68f1dbc7f3e5dff651fa75f8075c
nyaundi brian <danleyb2@gmail.com>
2020-08-19 18:27:14 +0300

    #487 Implement new landing page swipe pattern using IdeasExplorer

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,141 +1,19 @@
 <template>
-  <Layout>
-    <!--    <v-container>-->
-    <div id="newhomePage">
-      <!-- desktop view -->
-
-      <section id="hero">
-        <v-row>
-          <v-col>
-            <h1>Every Day, Log An Idea</h1>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col sm="6" cols="12" class="d-flex align-center">
-            <div>
-              <p>A project idea, a startup idea, a work idea.</p>
-              <p>
-                We'll send you a daily email reminding you to submit an idea.
-                Just respond to it and we'll save it for you.
-              </p>
-
-              <!-- header image only show above signup button on small viewports (1 column header) i.e. hide on larger viewport s-->
-              <v-row class="d-flex d-sm-none">
-                <v-col sm="6" cols="12" class="text-center">
-                  <img
-                    class="hero__image"
-                    src="~/assets/images/home/undraw_counting_stars_rrnl.svg"
-                  />
-                </v-col>
-              </v-row>
-
-              <!-- Signup Button -->
-              <div class="text-center text-sm-left">
-                <v-btn dark rounded class="wider" :to="{ name: 'auth-signup' }"
-                  >Sign Up
-                </v-btn>
-              </div>
-            </div>
-          </v-col>
-
-          <!-- header image only show to the right on wider viewports (2 column header) -->
-          <v-col sm="6" cols="12" class="justify-center d-none d-sm-flex">
-            <img
-              class="hero__image"
-              src="~/assets/images/home/undraw_counting_stars_rrnl.svg"
-            />
-          </v-col>
-        </v-row>
-      </section>
-
-      <section>
-        <h2 class="text-center">Browse Popular Ideas</h2>
-
-        <!-- Display idea cards -->
-        <v-row>
-          <v-col>
-            <v-row class="reviews">
-              <v-col v-for="idea in ideas" :key="idea.ideaId" cols="12" sm="4">
-                <ideas-list-idea
-                  :key="idea.ideaId"
-                  :idea="idea"
-                  :show-author="showAuthor"
-                ></ideas-list-idea>
-              </v-col>
-            </v-row>
-          </v-col>
-        </v-row>
-
-        <!-- See More Button -->
-        <div class="text-center">
-          <v-btn outlined rounded class="wider" :to="{ name: 'ideas-all' }"
-            >See More
-          </v-btn>
-        </div>
-      </section>
-
-      <section id="how-does-it-work">
-        <v-row>
-          <v-col cols="12" sm="6" class="text-center">
-            <img
-              class="img2"
-              src="~/assets/images/home/undraw_donut_love_kau1.svg"
-            />
-          </v-col>
-          <v-col cols="12" sm="6">
-            <div class="last-section__feature-list">
-              <h2 class="mb-4">How Does It Work?</h2>
-              <p>
-                <v-icon small>mdi-calendar-star</v-icon>
-                We send you an email to remind you to submit an idea.
-              </p>
-              <p>
-                <v-icon small>mdi-email-send-outline</v-icon>
-                You just respond to the email with an idea and we store it for
-                you.
-              </p>
-              <p>You'll also:</p>
-              <p>
-                <v-icon small>mdi-head-dots-horizontal-outline</v-icon>
-                Practice generating ideas to become an idea machine
-              </p>
-              <p>
-                <v-icon small>mdi-alarm</v-icon>
-                Get reminded of your best ideas later
-              </p>
-              <p>
-                <v-icon small>mdi-thought-bubble-outline</v-icon>
-                Browse other ideas to inspire your own creativity
-              </p>
-
-              <div class="text-center text-sm-left">
-                <v-btn dark rounded class="wider" :to="{ name: 'auth-signup' }"
-                  >Sign Up
-                </v-btn>
-              </div>
-            </div>
-          </v-col>
-        </v-row>
-      </section>
-
-      <main-footer />
-    </div>
-    <!--    </v-container>-->
+  <Layout container-full>
+    <idea-explorer />
   </Layout>
 </template>
 
 <script>
 import clip from 'text-clipper'
 import Layout from '@/components/layout/Layout'
-import MainFooter from '@/components/layout/MainFooter'
 import getPublicIdeas from '~/graphql/query/getPublicIdeas'
-import ideasListIdea from '~/components/ideasList/ideasListIdea'
+import IdeaExplorer from '@/components/IdeaExplorer'
 
 export default {
   components: {
-    ideasListIdea,
     Layout,
-    MainFooter
+    IdeaExplorer
   },
   async asyncData({ app }) {
     let result = await app.$amplifyApi.graphql({
