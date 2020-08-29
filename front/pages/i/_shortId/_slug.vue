cdbbc9ba9108bebdb1753338c0bd805208dd3ab2
nyaundi brian <danleyb2@gmail.com>
2020-08-29 20:35:44 +0300

    #487 eslint

--- front/pages/i/_shortId/_slug.vue
+++ front/pages/i/_shortId/_slug.vue
@@ -5,76 +5,76 @@
 </template>
 
 <script>
-  import clip from 'text-clipper'
-  import getIdea from '@/graphql/query/getIdea'
-  import IdeaExplorer from '@/components/IdeaExplorer'
-  import Layout from '@/components/layout/Layout'
+import clip from 'text-clipper'
+import getIdea from '@/graphql/query/getIdea'
+import IdeaExplorer from '@/components/IdeaExplorer'
+import Layout from '@/components/layout/Layout'
 
-  export default {
-    components: {
-      IdeaExplorer,
-      Layout
-    },
+export default {
+  components: {
+    IdeaExplorer,
+    Layout
+  },
 
-    async asyncData({ app, route, redirect, error, res, store }) {
-      try {
-        const { data } = await app.$amplifyApi.graphql({
-          query: getIdea,
-          variables: { shortId: route.params.shortId },
-          authMode: store.getters['userData/isAuthenticated']
-            ? undefined
-            : 'API_KEY'
-        })
-        const idea = data.getIdea
-
-        // Redirect to correct slug if it doesn't match
-        if (idea.slug !== route.params.slug) {
-          return redirect(301, `/i/${idea.shortId}/${idea.slug}`)
-        }
-
-        return { idea, ideaQueue: [idea] }
-      } catch (e) {
-        error({ statusCode: 404, message: 'Idea not found' })
-      }
-    },
-
-    data() {
-      return {
-        idea: null,
-        ideaQueue: []
-      }
-    },
-
-    head() {
-      const truncatedIdeaContent = clip(this.idea.content, 180, {
-        html: true,
-        maxLines: 8,
-        indicator: '... (see more)'
+  async asyncData({ app, route, redirect, error, res, store }) {
+    try {
+      const { data } = await app.$amplifyApi.graphql({
+        query: getIdea,
+        variables: { shortId: route.params.shortId },
+        authMode: store.getters['userData/isAuthenticated']
+          ? undefined
+          : 'API_KEY'
       })
-      const ogDescription = truncatedIdeaContent.replace(/<\/?[^>]+(>|$)/g, '')
-      const attrs = {
-        title: `${this.idea.title} - Daily Idea`,
-        meta: [
-          { hid: 'og:title', property: 'og:title', content: this.idea.title },
-          {
-            hid: 'og:description',
-            property: 'og:description',
-            content: ogDescription
-          }
-        ]
+      const idea = data.getIdea
+
+      // Redirect to correct slug if it doesn't match
+      if (idea.slug !== route.params.slug) {
+        return redirect(301, `/i/${idea.shortId}/${idea.slug}`)
       }
 
-      if (this.idea.previewImage) {
-        attrs.meta.push({
-          hid: 'og:image',
-          property: 'og:image',
-          content: `https://${process.env.USER_UPLOADS_S3_DOMAIN}.s3.amazonaws.com/${this.idea.previewImage}`
-        })
-      }
-
-      return attrs
+      return { idea, ideaQueue: [idea] }
+    } catch (e) {
+      error({ statusCode: 404, message: 'Idea not found' })
     }
+  },
+
+  data() {
+    return {
+      idea: null,
+      ideaQueue: []
+    }
+  },
+
+  head() {
+    const truncatedIdeaContent = clip(this.idea.content, 180, {
+      html: true,
+      maxLines: 8,
+      indicator: '... (see more)'
+    })
+    const ogDescription = truncatedIdeaContent.replace(/<\/?[^>]+(>|$)/g, '')
+    const attrs = {
+      title: `${this.idea.title} - Daily Idea`,
+      meta: [
+        { hid: 'og:title', property: 'og:title', content: this.idea.title },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: ogDescription
+        }
+      ]
+    }
+
+    if (this.idea.previewImage) {
+      attrs.meta.push({
+        hid: 'og:image',
+        property: 'og:image',
+        content: `https://${process.env.USER_UPLOADS_S3_DOMAIN}.s3.amazonaws.com/${this.idea.previewImage}`
+      })
+    }
+
+    return attrs
   }
+}
 </script>
 
 <style lang="scss" scoped></style>
