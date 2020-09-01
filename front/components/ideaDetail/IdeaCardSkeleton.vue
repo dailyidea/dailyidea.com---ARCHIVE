0e5de18c4945a5507f0bb546b8c28e8b957e9969
Vedmant <vedmant@gmail.com>
2020-09-01 18:54:15 +0300

    Use Vuetify skeleton component

--- front/components/ideaDetail/IdeaCardSkeleton.vue
+++ front/components/ideaDetail/IdeaCardSkeleton.vue
@@ -1,51 +1,13 @@
 <template>
   <div class="idea-card-skeleton">
     <div ref="page" align="stretch" class="elevation-2 ma-1 card">
-      <v-row>
-        <v-col cols="11" md="8">
-          <div class="placeholder"></div>
-        </v-col>
-      </v-row>
-      <v-row class="mb-6">
-        <v-col cols="2" md="1">
-          <div class="placeholder-dot"></div>
-        </v-col>
-        <v-col cols="4" md="2">
-          <div class="placeholder"></div>
-        </v-col>
-        <v-col cols="4" md="2">
-          <div class="placeholder"></div>
-        </v-col>
-      </v-row>
-      <v-row class="mb-10">
-        <v-col cols="3" md="2">
-          <div class="placeholder"></div>
-        </v-col>
-        <v-col cols="3" md="2">
-          <div class="placeholder"></div>
-        </v-col>
-        <v-col cols="3" md="2">
-          <div class="placeholder"></div>
-        </v-col>
-        <v-col cols="3" md="2">
-          <div class="placeholder"></div>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="11" md="7">
-          <div class="placeholder"></div>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="10" md="6">
-          <div class="placeholder"></div>
-        </v-col>
-      </v-row>
-      <v-row style="margin-top: 10rem;">
-        <v-col cols="11" md="7">
-          <div class="placeholder"></div>
-        </v-col>
-      </v-row>
+      <v-skeleton-loader
+        ref="skeleton"
+        :boilerplate="true"
+        type="card-heading, list-item-avatar, divider, list-item-three-line, image, actions"
+        :tile="false"
+        class="mx-auto"
+      ></v-skeleton-loader>
     </div>
   </div>
 </template>
@@ -79,20 +41,5 @@ export default {
     width: 65vw;
     height: 80vh;
   }
-
-  /* .rounded doesn't work because i'm applying this to a .row (which i shouldn't) */
-}
-
-.placeholder {
-  width: 100%;
-  height: 20px;
-  background: #e3dfe9;
-}
-
-.placeholder-dot {
-  width: 20px;
-  height: 20px;
-  background: #e3dfe9;
-  border-radius: 100%;
 }
 </style>
