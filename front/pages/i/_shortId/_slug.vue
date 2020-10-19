94f7390756bcdf4f9465a82a8da59f3454d6a9f1
Vedmant <vedmant@gmail.com>
2020-10-19 13:03:10 +0300

    Errors when swiping right fast from index page on mobile, disable explainer on SSR

--- front/pages/i/_shortId/_slug.vue
+++ front/pages/i/_shortId/_slug.vue
@@ -120,10 +120,6 @@ export default {
     })
   },
 
-  created() {
-    this.showExplainer = !Cookies.get('hasSeenExplainer')
-  },
-
   mounted() {
     this.cacheIdeas(this.getCategoryFromURL())
     this.incrementViews()
@@ -133,6 +129,7 @@ export default {
       this.showIdeaPostedDialog = true
       // this.updateCreatedIdea(null)
     }
+    this.showExplainer = process.client && !Cookies.get('hasSeenExplainer')
   },
 
   methods: {
@@ -179,6 +176,10 @@ export default {
 
       if (this.ideaIndex >= this.ideaQueue.length || this.ideaIndex < 0) {
         this.ideaIndex -= direction
+      }
+
+      if (!this.ideaQueue[this.ideaIndex]) {
+        return
       }
 
       this.idea = this.ideaQueue[this.ideaIndex]
@@ -230,6 +231,9 @@ export default {
     },
 
     animationOutEnd() {
+      if (this.showExplainer) {
+        Cookies.set('hasSeenExplainer', 1, { expires: 365 })
+      }
       this.showExplainer = false
     }
   },
