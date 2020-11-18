068e72175be20309f61c252360436cf3a7e26a40
Vedmant <vedmant@gmail.com>
2020-11-18 18:12:11 +0200

    expired link page fonts are way too big #721

---
+++ front/components/layout/CardLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <layout>
+    <card class="layout-card">
+      <slot />
+    </card>
+  </layout>
+</template>
+
+<script>
+import Layout from '@/components/layout/Layout'
+import Card from '@/components/shared/Card'
+
+export default {
+  components: { Layout, Card }
+}
+</script>
+
+<style lang="scss" scoped>
+.layout-card {
+  margin: 0 auto;
+
+  @media (min-width: $screen-md-min) {
+    min-width: 70vw;
+    max-width: 70vw;
+    left: calc(50% - 35vw);
+  }
+}
+</style>
--- front/pages/auth/unathorized.vue
+++ front/pages/auth/unathorized.vue
@@ -1,57 +1,48 @@
 <template>
-  <Layout>
-    <card
-      :additional-styling="{
-        'min-height': '60vh',
-        'max-width': isMobile ? '' : '60vw'
-      }"
-    >
-      <div class="w-full text-center pt-10 pb-10">
-        <div class="m-auto" style="max-width: 329px;">
-          <img
-            src="~assets/images/auth/unathorized.png"
-            width="329"
-            height="244"
-            alt="Unathorized"
-            class="mb-5"
-          />
-          <p class="mb-5">
-            Oops, looks like you don’t have an account yet! Lets help you out
-            with that.
-          </p>
-          <v-btn
-            color="primary"
-            rounded
-            large
-            to="/auth/signup"
-            class="col-12 signup-button"
-            >Sign Up</v-btn
-          >
-          <nuxt-link
-            class="d-inline-block link-highlight text-lg text-center mt-6"
-            :to="{ name: 'auth-login' }"
-          >
-            Login
-          </nuxt-link>
-        </div>
+  <card-layout>
+    <div class="w-full text-center pt-10 pb-10">
+      <div class="m-auto" style="max-width: 329px;">
+        <img
+          src="~assets/images/auth/unathorized.png"
+          width="329"
+          height="244"
+          alt="Unathorized"
+          class="mb-5"
+        />
+        <p class="mb-5">
+          Oops, looks like you don’t have an account yet! Lets help you out with
+          that.
+        </p>
+        <v-btn
+          color="primary"
+          rounded
+          large
+          to="/auth/signup"
+          class="col-12 signup-button"
+          >Sign Up</v-btn
+        >
+        <nuxt-link
+          class="d-inline-block link-highlight text-lg text-center mt-6"
+          :to="{ name: 'auth-login' }"
+        >
+          Login
+        </nuxt-link>
       </div>
-      <div class="w-full">
-        <swipe-footer />
-      </div>
-    </card>
-  </Layout>
+    </div>
+    <div class="w-full">
+      <swipe-footer />
+    </div>
+  </card-layout>
 </template>
 
 <script>
-import Layout from '@/components/layout/Layout'
-import Card from '@/components/shared/Card'
 import SwipeFooter from '@/components/layout/SwipeFooter'
+import CardLayout from '@/components/layout/CardLayout'
 
 export default {
   components: {
-    SwipeFooter,
-    Card,
-    Layout
+    CardLayout,
+    SwipeFooter
   }
 }
 </script>
