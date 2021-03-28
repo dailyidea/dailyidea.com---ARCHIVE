ac8535cc8a0eaf04c321dd7b8199a807d01c2419
Vedmant <vedmant@gmail.com>
2021-03-28 15:46:02 +0300

    Update default "unauthorized" page #927

--- front/pages/auth/unathorized.vue
+++ front/pages/auth/unathorized.vue
@@ -1,65 +1,42 @@
 <template>
   <card-layout>
     <div class="w-full text-center pt-10 pb-10">
-      <div class="m-auto" style="max-width: 329px;">
+      <div class="m-auto text-center">
         <img
-          src="~assets/images/auth/unathorized.png"
-          width="329"
-          height="244"
+          src="~assets/images/auth/unathorized.svg"
+          width="592"
+          height="376"
           alt="Unathorized"
           class="mb-5"
         />
-        <p class="mb-5">
-          Oops, looks like you don’t have an account yet! Lets help you out with
-          that.
-        </p>
-        <v-btn
-          color="primary"
-          rounded
-          large
-          to="/auth/signup"
-          class="col-12 signup-button"
-          >Sign Up</v-btn
-        >
+        <h1 class="mb-5 heading">
+          Oops you’re not logged in.
+        </h1>
+        <p>Let's help you out with that.</p>
         <nuxt-link
-          class="d-inline-block link-highlight text-lg text-center mt-6"
+          class="d-inline-block link-highlight text-center"
           :to="{ name: 'auth-login' }"
         >
-          Login
+          log in or sign up
         </nuxt-link>
       </div>
-    </div>
-    <div class="w-full">
-      <swipe-footer />
     </div>
   </card-layout>
 </template>
 
 <script>
-import SwipeFooter from '@/components/layout/SwipeFooter'
 import CardLayout from '@/components/layout/CardLayout'
 
 export default {
   components: {
-    CardLayout,
-    SwipeFooter
+    CardLayout
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.signup-button.v-btn.v-size--large {
-  padding-top: 2px;
-  font-size: 1.25rem;
-  text-transform: none;
-}
-::v-deep {
-  footer {
-    padding-top: 10px;
-  }
-
-  .col.col-12 {
-    padding: 3px 20px;
-  }
+.heading {
+  font-size: 30px;
+  color: #4c4763;
 }
 </style>
